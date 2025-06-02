8bd195e5220ab1084d0b4b00d1aa854bba8ea9a5
xt0032rus <andrey.denikin9@mail.com>
2025-06-02 23:08:31 +0300

    fuxi: Update from OS2.0.200.9.VMCCNXM release-keys
    Signed-off-by: xt0032rus <andrey.denikin9@gmail.com>

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -37,3 +37,6 @@ PRODUCT_SYSTEM_NAME := Xiaomi 13
 PRODUCT_SYSTEM_DEVICE := Xiaomi 13
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=Xiaomi/fuxi/fuxi:15/AQ3A.240912.001/OS2.0.200.9.VMCCNXM:user/release-keys
