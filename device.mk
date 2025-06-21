2b365a72b3650ed08f91f97b0411676b8e5615be
xt0032rus <andrey.denikin9@mail.com>
2025-06-21 17:16:20 +0300

    fuxi: Add MiuiCamera

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from sm8550-common
 $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 
+# Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
